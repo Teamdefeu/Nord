399c75266cf927477c7ddcd5e2e34db864613e25
Le Monochrome <lemonochrome@users.noreply.github.com>
2020-02-20 12:10:55 -0500

    Recherche - nouveau modèle
    onfocus: élargi vers la gauche

--- js/nord.js
+++ js/nord.js
@@ -1,25 +1,5 @@
 ! function($) {
     $(document).ready(function() {
 
-        //-- Search icon
-        (function() {
-
-            $(".open-form").click(function(){
-                $(".open-form").hide();
-                $(".close-form").css("display","block");
-                $(".search-block-form").show();
-                $(".search-block-form input").focus();
-                return false;
-            });
-            $(".close-form").click(function(){
-                $(".close-form").hide();
-                $(".open-form").css("display","block");
-                $(".search-block-form").hide();
-                return false;
-            });
-
-        })();
-
-
     });
 }(jQuery);
